1fb70b63bac38828e484258c900fefe674f13c1f
Izhar <eezhar@gmail.com>
2023-06-01 13:10:39 +0800

    clean up app.tsx file

---
+++ client/tutorial/Chair.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+type Props = {};
+
+const Chair = (props: Props) => {
+  return (
+    <View>
+      <Text>This is char</Text>
+    </View>
+  );
+};
+
+export default Chair;
+
+const styles = StyleSheet.create({});
